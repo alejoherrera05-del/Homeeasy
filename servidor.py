c91238bf4d0c7d9233f9e3af1ee5adf0e0382996
alejoherrera05-del <alejoherrera05@gmail.com>
2026-08-27 23:08:56 -0500

    Tighten Hommy CORS response headers

--- servidor.py
+++ servidor.py
@@ -45,7 +45,6 @@ def cors_headers(response):
     if origin and origin in allowed_origins():
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Vary"] = "Origin"
-        response.headers["Access-Control-Allow-Credentials"] = "false"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-HomeEasy-Session, X-HomeEasy-Meta"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
         response.headers["Access-Control-Max-Age"] = "600"
